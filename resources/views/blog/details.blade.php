978ed7275a2850f4af1f9edb5d44cd35b0fc1743
jaleel <jaleelzhang@gmail.com>
2017-03-02 22:44:56 +0800

    debug

--- resources/views/blog/details.blade.php
+++ resources/views/blog/details.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <div class="jumbotron">
-    <h1>{{ $details['title'] }}</h1>
-    <p style="font-size: larger">{{ $details['content'] }}</p>
+    <h1 style="font-size: x-large; color: #636b6f">{{ $details['title'] }}</h1>
+    <p style="font-size: larger">{!! $details['content'] !!}</p>
 </div>
 @endsection

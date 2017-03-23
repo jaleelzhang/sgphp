547977670bd7cc1688522c6b5fbea0bfc4caeb5a
jaleel <jaleelzhang@gmail.com>
2017-03-23 21:12:57 +0800

    debug

--- app/Http/Controllers/blog/BlogController.php
+++ app/Http/Controllers/blog/BlogController.php
@@ -16,11 +16,9 @@ class BlogController extends Controller
     public function index($type = '')
     {
         if (empty($type)) {
-//            $blogs = Blog::orderBy('create_time', 'desc')->paginate(10);
-            $blogs = Blog::paginate(10);
+            $blogs = Blog::orderBy('create_time', 'desc')->paginate(10);
         } else {
-//            $blogs = Blog::where('type', $type)->orderBy('create_time', 'desc')->paginate(10);
-            $blogs = Blog::where('type', $type)->paginate(10);
+            $blogs = Blog::where('type', $type)->orderBy('create_time', 'desc')->paginate(10);
         }
 
         return view('blog/bloglist')->withblogs($blogs);
--- resources/views/blog/bloglist.blade.php
+++ resources/views/blog/bloglist.blade.php
@@ -6,12 +6,6 @@
 
 @section('content')
     @foreach($blogs as $blog)
-        {{--<div class="jumbotron">
-            <h1><a href="{{ url('blog/' . $blog->_id) }}" style="font-size: x-large; color: #636b6f">{{ $blog->title }}</a></h1>
-            <p>{{ date('Y年m月d日 H:i:s', $blog->create_time) }}</p>
-            <p style="font-size: larger">{{ $blog->abstract }}</p>
-            <p><a class="btn btn-default btn-lg" href="{{ url('blog/' . $blog->_id) }}" role="button">Learn more</a></p>
-        </div>--}}
         <blockquote style="border-left: 5pt solid lightgrey">
             <h1><a href="{{ url('blog/' . $blog->_id) }}" style="font-size: x-large; color: #636b6f">{{ $blog->title }}</a></h1>
             <small>{{ date('Y年m月d日 H:i:s', $blog->create_time) }}</small>

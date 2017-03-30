22f58f603b45f9254faaa2a4302f3ec894588cd5
jaleel <jaleelzhang@gmail.com>
2017-03-30 15:59:24 +0800

    debug

--- app/Http/Controllers/blog/BlogController.php
+++ app/Http/Controllers/blog/BlogController.php
@@ -16,9 +16,9 @@ class BlogController extends Controller
     public function index($type = '')
     {
         if (empty($type)) {
-            $blogs = Blog::orderBy('create_time', 'desc')->paginate(10);
+            $blogs = Blog::orderBy('create_time', 'desc')->paginate(5);
         } else {
-            $blogs = Blog::where('type', $type)->orderBy('create_time', 'desc')->paginate(10);
+            $blogs = Blog::where('type', $type)->orderBy('create_time', 'desc')->paginate(5);
         }
 
         return view('blog/bloglist')->withblogs($blogs);
